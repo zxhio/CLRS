10fa06832107fbe89df112294e70f50a6f8bccd6
zxhio <definezxh@163.com>
2019-02-19 00:50:16 +0800

    timestamp

---
+++ clrs.h
@@ -0,0 +1,7 @@
+#include <chrono>
+
+uint64_t timestamp() {
+    return std::chrono::duration_cast<std::chrono::microseconds>(
+               std::chrono::high_resolution_clock::now().time_since_epoch())
+        .count();
+}
